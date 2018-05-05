fb5c869753d60367ee80c02508cf25fb7807d765
Fabio de Matos <fmatosqg@gmail.com>
2018-05-05 13:34:08 +1000

    Improvements on var name generation

---
+++ examples/flutter_gallery/lib/gen/assetHolder.g.dart
@@ -0,0 +1,398 @@
+/// My favorite class. asset: lib/gallery/example_code.dart
+/// My favorite class. asset: packages/flutter_gallery_assets/white_logo/logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/white_logo/1.5x/logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/white_logo/2.5x/logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/white_logo/3.0x/logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/white_logo/4.0x/logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/videos/butterfly.mp4
+/// My favorite class. asset: packages/flutter_gallery_assets/animated_flutter_lgtm.gif
+/// My favorite class. asset: packages/flutter_gallery_assets/animated_flutter_stickers.webp
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar_background.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer0.png
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer1.png
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer2.png
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer3.png
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer4.png
+/// My favorite class. asset: packages/flutter_gallery_assets/appbar/appbar_background_layer5.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/avatar.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image10.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image11.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image12.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image13.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image1.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image2.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image3.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image4.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image5.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image6.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image7.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image8.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/image9.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/logo_small.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/logo_medium.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/fish.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/healthy.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/main.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/meat.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/quick.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/spicy.png
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/veggie.png
+/// My favorite class. asset: packages/flutter_gallery_assets/about_logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/ali_connors.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/drawer_logo.png
+/// My favorite class. asset: packages/flutter_gallery_assets/kangaroo_valley_safari.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/top_10_australian_beaches.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_0.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_1.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_2.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_3.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_4.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_5.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_6.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_7.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_8.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_9.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_10.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/landscape_11.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/backpack.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/beachball.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/binoculars.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/brush.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/chair.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/chucks.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/clock.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/fish_bowl.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/flippers.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/green-shoes.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/heels.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/helmet.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/lawn_chair.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/lipstick.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/popsicle.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/radio.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/sunnies.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/surfboard.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/products/teapot.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/vendors/16c477b.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/vendors/ali-connors.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/vendors/peter-carlsson.png
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/vendors/sandra-adams.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/vendors/zach.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/cupertino_navigation/person1.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/cupertino_navigation/person2.jpg
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/fonts/Raleway-Regular.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/fonts/Raleway-Medium.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/pesto/fonts/Raleway-SemiBold.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/shrine/fonts/abrilfatface/AbrilFatface-Regular.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GalleryIcons.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-BoldItalic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-Bold.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-Italic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-MediumItalic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-Medium.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSans-Regular.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-BoldItalic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Bold.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Italic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-MediumItalic.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Medium.ttf
+/// My favorite class. asset: packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Regular.ttf
+class test {
+  static const String exampleCodeDart = 'lib/gallery/example_code.dart';
+
+  static const String logoPng =
+      'packages/flutter_gallery_assets/white_logo/logo.png';
+
+  static const String logoPng =
+      'packages/flutter_gallery_assets/white_logo/1.5x/logo.png';
+
+  static const String logoPng =
+      'packages/flutter_gallery_assets/white_logo/2.5x/logo.png';
+
+  static const String logoPng =
+      'packages/flutter_gallery_assets/white_logo/3.0x/logo.png';
+
+  static const String logoPng =
+      'packages/flutter_gallery_assets/white_logo/4.0x/logo.png';
+
+  static const String butterflyMp4 =
+      'packages/flutter_gallery_assets/videos/butterfly.mp4';
+
+  static const String animatedFlutterLgtmGif =
+      'packages/flutter_gallery_assets/animated_flutter_lgtm.gif';
+
+  static const String animatedFlutterStickersWebp =
+      'packages/flutter_gallery_assets/animated_flutter_stickers.webp';
+
+  static const String appbarBackgroundJpg =
+      'packages/flutter_gallery_assets/appbar_background.jpg';
+
+  static const String appbarBackgroundLayer0Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer0.png';
+
+  static const String appbarBackgroundLayer1Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer1.png';
+
+  static const String appbarBackgroundLayer2Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer2.png';
+
+  static const String appbarBackgroundLayer3Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer3.png';
+
+  static const String appbarBackgroundLayer4Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer4.png';
+
+  static const String appbarBackgroundLayer5Png =
+      'packages/flutter_gallery_assets/appbar/appbar_background_layer5.png';
+
+  static const String avatarJpg =
+      'packages/flutter_gallery_assets/pesto/avatar.jpg';
+
+  static const String image10Jpg =
+      'packages/flutter_gallery_assets/pesto/image10.jpg';
+
+  static const String image11Jpg =
+      'packages/flutter_gallery_assets/pesto/image11.jpg';
+
+  static const String image12Jpg =
+      'packages/flutter_gallery_assets/pesto/image12.jpg';
+
+  static const String image13Jpg =
+      'packages/flutter_gallery_assets/pesto/image13.jpg';
+
+  static const String image1Jpg =
+      'packages/flutter_gallery_assets/pesto/image1.jpg';
+
+  static const String image2Jpg =
+      'packages/flutter_gallery_assets/pesto/image2.jpg';
+
+  static const String image3Jpg =
+      'packages/flutter_gallery_assets/pesto/image3.jpg';
+
+  static const String image4Jpg =
+      'packages/flutter_gallery_assets/pesto/image4.jpg';
+
+  static const String image5Jpg =
+      'packages/flutter_gallery_assets/pesto/image5.jpg';
+
+  static const String image6Jpg =
+      'packages/flutter_gallery_assets/pesto/image6.jpg';
+
+  static const String image7Jpg =
+      'packages/flutter_gallery_assets/pesto/image7.jpg';
+
+  static const String image8Jpg =
+      'packages/flutter_gallery_assets/pesto/image8.jpg';
+
+  static const String image9Jpg =
+      'packages/flutter_gallery_assets/pesto/image9.jpg';
+
+  static const String logoSmallPng =
+      'packages/flutter_gallery_assets/pesto/logo_small.png';
+
+  static const String logoMediumPng =
+      'packages/flutter_gallery_assets/pesto/logo_medium.png';
+
+  static const String fishPng =
+      'packages/flutter_gallery_assets/pesto/fish.png';
+
+  static const String healthyPng =
+      'packages/flutter_gallery_assets/pesto/healthy.png';
+
+  static const String mainPng =
+      'packages/flutter_gallery_assets/pesto/main.png';
+
+  static const String meatPng =
+      'packages/flutter_gallery_assets/pesto/meat.png';
+
+  static const String quickPng =
+      'packages/flutter_gallery_assets/pesto/quick.png';
+
+  static const String spicyPng =
+      'packages/flutter_gallery_assets/pesto/spicy.png';
+
+  static const String veggiePng =
+      'packages/flutter_gallery_assets/pesto/veggie.png';
+
+  static const String aboutLogoPng =
+      'packages/flutter_gallery_assets/about_logo.png';
+
+  static const String aliConnorsJpg =
+      'packages/flutter_gallery_assets/ali_connors.jpg';
+
+  static const String drawerLogoPng =
+      'packages/flutter_gallery_assets/drawer_logo.png';
+
+  static const String kangarooValleySafariJpg =
+      'packages/flutter_gallery_assets/kangaroo_valley_safari.jpg';
+
+  static const String top10AustralianBeachesJpg =
+      'packages/flutter_gallery_assets/top_10_australian_beaches.jpg';
+
+  static const String landscape0Jpg =
+      'packages/flutter_gallery_assets/landscape_0.jpg';
+
+  static const String landscape1Jpg =
+      'packages/flutter_gallery_assets/landscape_1.jpg';
+
+  static const String landscape2Jpg =
+      'packages/flutter_gallery_assets/landscape_2.jpg';
+
+  static const String landscape3Jpg =
+      'packages/flutter_gallery_assets/landscape_3.jpg';
+
+  static const String landscape4Jpg =
+      'packages/flutter_gallery_assets/landscape_4.jpg';
+
+  static const String landscape5Jpg =
+      'packages/flutter_gallery_assets/landscape_5.jpg';
+
+  static const String landscape6Jpg =
+      'packages/flutter_gallery_assets/landscape_6.jpg';
+
+  static const String landscape7Jpg =
+      'packages/flutter_gallery_assets/landscape_7.jpg';
+
+  static const String landscape8Jpg =
+      'packages/flutter_gallery_assets/landscape_8.jpg';
+
+  static const String landscape9Jpg =
+      'packages/flutter_gallery_assets/landscape_9.jpg';
+
+  static const String landscape10Jpg =
+      'packages/flutter_gallery_assets/landscape_10.jpg';
+
+  static const String landscape11Jpg =
+      'packages/flutter_gallery_assets/landscape_11.jpg';
+
+  static const String backpackPng =
+      'packages/flutter_gallery_assets/shrine/products/backpack.png';
+
+  static const String beachballPng =
+      'packages/flutter_gallery_assets/shrine/products/beachball.png';
+
+  static const String binocularsPng =
+      'packages/flutter_gallery_assets/shrine/products/binoculars.png';
+
+  static const String brushPng =
+      'packages/flutter_gallery_assets/shrine/products/brush.png';
+
+  static const String chairPng =
+      'packages/flutter_gallery_assets/shrine/products/chair.png';
+
+  static const String chucksPng =
+      'packages/flutter_gallery_assets/shrine/products/chucks.png';
+
+  static const String clockPng =
+      'packages/flutter_gallery_assets/shrine/products/clock.png';
+
+  static const String fishBowlPng =
+      'packages/flutter_gallery_assets/shrine/products/fish_bowl.png';
+
+  static const String flippersPng =
+      'packages/flutter_gallery_assets/shrine/products/flippers.png';
+
+  static const String greenShoesPng =
+      'packages/flutter_gallery_assets/shrine/products/green-shoes.png';
+
+  static const String heelsPng =
+      'packages/flutter_gallery_assets/shrine/products/heels.png';
+
+  static const String helmetPng =
+      'packages/flutter_gallery_assets/shrine/products/helmet.png';
+
+  static const String lawnChairPng =
+      'packages/flutter_gallery_assets/shrine/products/lawn_chair.png';
+
+  static const String lipstickPng =
+      'packages/flutter_gallery_assets/shrine/products/lipstick.png';
+
+  static const String popsiclePng =
+      'packages/flutter_gallery_assets/shrine/products/popsicle.png';
+
+  static const String radioPng =
+      'packages/flutter_gallery_assets/shrine/products/radio.png';
+
+  static const String sunniesPng =
+      'packages/flutter_gallery_assets/shrine/products/sunnies.png';
+
+  static const String surfboardPng =
+      'packages/flutter_gallery_assets/shrine/products/surfboard.png';
+
+  static const String teapotPng =
+      'packages/flutter_gallery_assets/shrine/products/teapot.png';
+
+  static const String _16c477bJpg =
+      'packages/flutter_gallery_assets/shrine/vendors/16c477b.jpg';
+
+  static const String aliConnorsPng =
+      'packages/flutter_gallery_assets/shrine/vendors/ali-connors.png';
+
+  static const String peterCarlssonPng =
+      'packages/flutter_gallery_assets/shrine/vendors/peter-carlsson.png';
+
+  static const String sandraAdamsJpg =
+      'packages/flutter_gallery_assets/shrine/vendors/sandra-adams.jpg';
+
+  static const String zachJpg =
+      'packages/flutter_gallery_assets/shrine/vendors/zach.jpg';
+
+  static const String person1Jpg =
+      'packages/flutter_gallery_assets/cupertino_navigation/person1.jpg';
+
+  static const String person2Jpg =
+      'packages/flutter_gallery_assets/cupertino_navigation/person2.jpg';
+
+  static const String ralewayRegularTtf =
+      'packages/flutter_gallery_assets/pesto/fonts/Raleway-Regular.ttf';
+
+  static const String ralewayMediumTtf =
+      'packages/flutter_gallery_assets/pesto/fonts/Raleway-Medium.ttf';
+
+  static const String ralewaySemiBoldTtf =
+      'packages/flutter_gallery_assets/pesto/fonts/Raleway-SemiBold.ttf';
+
+  static const String abrilFatfaceRegularTtf =
+      'packages/flutter_gallery_assets/shrine/fonts/abrilfatface/AbrilFatface-Regular.ttf';
+
+  static const String galleryIconsTtf =
+      'packages/flutter_gallery_assets/fonts/GalleryIcons.ttf';
+
+  static const String googleSansBoldItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-BoldItalic.ttf';
+
+  static const String googleSansBoldTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-Bold.ttf';
+
+  static const String googleSansItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-Italic.ttf';
+
+  static const String googleSansMediumItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-MediumItalic.ttf';
+
+  static const String googleSansMediumTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-Medium.ttf';
+
+  static const String googleSansRegularTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSans-Regular.ttf';
+
+  static const String googleSansDisplayBoldItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-BoldItalic.ttf';
+
+  static const String googleSansDisplayBoldTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Bold.ttf';
+
+  static const String googleSansDisplayItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Italic.ttf';
+
+  static const String googleSansDisplayMediumItalicTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-MediumItalic.ttf';
+
+  static const String googleSansDisplayMediumTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Medium.ttf';
+
+  static const String googleSansDisplayRegularTtf =
+      'packages/flutter_gallery_assets/fonts/GoogleSansDisplay-Regular.ttf';
+
+  void eat() => print('Yum!');
+}
